8d941a6666fbc5c82bf8f54c7948b1a93895a210
Mighty! <116295241+mighty2dead@users.noreply.github.com>
2024-03-08 08:35:41 +0000

    Added vaja.py from programing class.

---
+++ School/vaja.py
@@ -0,0 +1,10 @@
+#Created: 03.08.2024 at 9:30
+
+def zaokrozi(znesek):
+
+    return round(znesek, 10)
+
+print(zaokrozi(8.89453 + 0.45378))
+print(zaokrozi(45))
+print(zaokrozi(69))
+print(zaokrozi(11))
